fb66307235286b6d14c5c24e55c089ffa34d5578
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-03-02 11:35:33 +0100

    Compatibility with Django 1.9+

--- agon_ratings/urls.py
+++ agon_ratings/urls.py
@@ -1,9 +1,7 @@
-try:
-    from django.conf.urls import url, patterns
-except ImportError:
-    from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url
 
+from . import views
 
-urlpatterns = patterns("agon_ratings.views",
-    url(r"^(?P<content_type_id>\d+)/(?P<object_id>\d+)/rate/$", "rate", name="agon_ratings_rate"),
-)
+urlpatterns = [  # "agon_ratings.views",
+    url(r"^(?P<content_type_id>\d+)/(?P<object_id>\d+)/rate/$", views.rate, name="agon_ratings_rate"),
+]
